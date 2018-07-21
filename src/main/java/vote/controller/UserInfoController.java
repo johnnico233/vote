d237d865787990b0f7f903c72f011a22f1e7e952
Yosoro <1011206880@qq.com>
2018-07-21 17:27:22 +0800

    commit by yosoro

---
+++ src/main/java/vote/controller/UserInfoController.java
@@ -0,0 +1,13 @@
+package vote.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserInfoController {
+    @RequestMapping("")
+    public String getUrl(){
+        return "user";
+    }
+}
